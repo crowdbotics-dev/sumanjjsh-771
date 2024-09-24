3d5780ec2a2160bad8bbf9a8881920b4f69b9b8c
Crowdbotics <engineering@crowdbotics.com>
2024-09-24 16:11:47 +0000

    Initial commit

---
+++ backend/sumanjjsh_771/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for sumanjjsh_771 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "sumanjjsh_771.settings"
+)
+
+application = get_wsgi_application()
